0d54f386fe9f53cfd4854031803633b720bbc2b9
Giveee <mphala26@gmail.com>
2024-07-05 14:09:08 +0200

    Add files via upload

---
+++ number.dart
@@ -0,0 +1,13 @@
+void main(){
+  int number =5;
+  if(number <10){
+  print("$number is greater than 10");
+}else{
+print("$number is not greater than 10");
+}
+if(5<10){
+print("$number is less than 10");
+}else{
+print("$number is not less than 10");
+}
+} 
